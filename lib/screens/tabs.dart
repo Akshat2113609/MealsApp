9f2b6bf75e98bd00b6d4d44cc63edd32ad802b9e
akshatawasthi21 <akshatawasthi21@gmail.com>
2024-08-13 05:53:56 +0000

    added drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:myapp/screens/categories.dart';
 import 'package:myapp/screens/meals.dart';
